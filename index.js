3371bda26aa5ee35795c0c794e498510857026ca
Max Rashes <max.j.rashes@pwc.com>
2019-09-12 23:07:33 -0500

    implemented question catcher

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
+
+const questions = [];
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use( bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/list', (req, res) => res.send(questions))
+  .get('/addToList', (req, res) => {
+    questions.push(req.query)
+    res.render('pages/index')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
